fe9b5d1393a2ecbff304bff477eb54765eb3ec44
Puzzlemaker <Puzzlemaker@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2008-05-17 16:52:01 +0000

    Whooops, forgot these

---
+++ branches/31104branch/src/organization/testdriver.cpp
@@ -0,0 +1,52 @@
+#include <externs.h>
+#include "testdriver.h"
+
+
+
+void testdriver()
+{
+	gOrgHandler = orgHandler();
+	//The org Mom creates the organizations.
+	organization *orgMom;
+
+	organization conservative("conservative");
+	conservative.alignment = ALIGN_CONSERVATIVE;
+	conservative.attackPower = 9001; //It's over 9000!!!!!!!!!!!!
+	organization liberal("liberal");
+	liberal.alignment = ALIGN_LIBERAL;
+	liberal.attackPower = 10;
+
+	for(int i = 0; i < VIEWNUM; i++)
+	{
+		liberal.specialInterests.push_back((enum Views)i);
+		conservative.specialInterests.push_back((enum Views)i);
+	}
+	gOrgHandler.addOrg(liberal);
+	gOrgHandler.addOrg(conservative);
+	string newName;
+	for(int j = 0; j < 10; j++)
+	{
+		newName = "Test" + (j+'0');
+		orgMom = new organization(newName);
+		if(LCSrandom(2))
+		{
+			orgMom->alignment = ALIGN_CONSERVATIVE;
+		}
+		else
+		{
+			orgMom->alignment = ALIGN_LIBERAL;
+		}
+		for(int k = 0; k < 4; k++)
+		{
+			orgMom->specialInterests.push_back((enum Views)LCSrandom(VIEWNUM));
+		}
+		gOrgHandler.addOrg(*orgMom);
+	}
+	
+	move(0,0);
+	addstr("Test Driver Completed");
+	//refresh();
+	//getch();
+
+	//gOrgHandler.addOrg();
+}
