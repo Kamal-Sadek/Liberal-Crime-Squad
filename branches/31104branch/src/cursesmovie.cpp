bbed4fc4656fc2bb1ecb63a028b3b3edc933d498
jonathansfox <jonathansfox@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2008-05-22 03:02:13 +0000

    altered cursesmovie.cpp for linux compile

--- branches/31104branch/src/cursesmovie.cpp
+++ branches/31104branch/src/cursesmovie.cpp
@@ -165,7 +165,7 @@ void filelistst::smartappend(filelistst &list2)
 
 	   for(int l=0;l<list.size();l++)
 	   {
-	      if(!_stricmp(list2.list[l2],list[l]))
+	      if(!strcmp(list2.list[l2],list[l]))
 		   {
 		      conf=0;
 		      break;
