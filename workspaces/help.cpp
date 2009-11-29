572c566a4cae983a5a1aff022b55dc23e6fdbeac
Firewolf123 <Firewolf123@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2009-11-29 20:43:17 +0000

    Partially implemented the in game help on the activities section. Will try to finish it off slowly over the next 4-5 days.
    Feel free to add to or beef up the look and feel in the help.cpp file

---
+++ workspaces/help.cpp
@@ -0,0 +1,113 @@
+#include <externs.h>
+
+// Help pages are drawn here.
+
+// NOTE: This will probably need updating if there are any fundamental code changes
+// that will change gameplay.
+void help(const char* string)
+{
+	erase();
+}
+
+// Generates help page on activities.
+// Just supply Activity type.
+void HelpActivities(int activityType)
+{
+	if(activityType == ACTIVITY_NONE)
+	{
+		return;
+	}
+
+	erase();
+	set_color(COLOR_WHITE,COLOR_BLACK,0);
+	move(0,0);
+	addstr("Liberal help on: ");
+
+
+	if(activityType == ACTIVITY_COMMUNITYSERVICE)
+	{
+		addstr("===Community service===");
+		move(2,1);
+		addstr("Has a minor effect on public opinion, increases Juice and has the chance");
+		move(3,1);
+		addstr("of reducing the activists Heat.");
+	}
+	else if(activityType == ACTIVITY_TROUBLE)
+	{
+		addstr("===Liberal Disobedience===");
+		move(2,1);
+		addstr("Liberal Disobedience is an illegal activity and a form of Liberal Activism"); 
+		move(3,1);
+		addstr("which has a positive effect on Public Opinion. It has very short prison");	
+		move(4,1);
+		addstr("sentences if you get convicted. It also increases juice.");	
+
+		move(6,1);
+		addstr("Persuasion, Charisma, Agility, Heart, Strength, Intelligence will increase the");	
+		move(7,1);
+		addstr("effect.");
+
+		move(9,1);
+		addstr("There is a chance that someone will phone to police. Agility, intelligence,");
+		move(10,1);
+		addstr("disguise and street sense will shake them off the scent.");
+
+	} 
+	else if(activityType == ACTIVITY_POLLS)
+	{
+		addstr("===Search Opinion Polls===");
+		move(2,1);
+		addstr("Search Opinion Polls is a legal activity that lets you see what people think");
+		move(3,1);
+		addstr("about various Issues that are part of Public Opinion.");
+
+		move(5,1);
+		addstr("Skill in computers and intelligence will give better results.");
+	}
+	else if(activityType == ACTIVITY_DOS_ATTACKS)
+	{
+		addstr("===Harass Websites===");
+		move(2,1);
+		addstr("Harass Websites is an illegal activity and a form of Liberal Activism. It");
+		move(3,1);
+		addstr("is a weaker version of Hacking. Much shorter prison sentences, is much");
+		move(4,1);
+		addstr("less likely to provoke a Raid by the Police or other law enforcement, and");
+		move(5,1);
+		addstr("only requires a Computers skill of 1.");
+
+		move(7,1);
+		addstr("Utilises only the Computers skill.");
+	}
+	else if(activityType == ACTIVITY_HACKING)
+	{
+		addstr("===Hacking===");
+		move(2,1);
+		addstr("Hacking is an activity, the most highly illegal form of Liberal Activism,");
+		move(3,1);
+		addstr("which has a good effect on public opinion, carrying very long prison");
+		move(4,1);
+		addstr("sentences.");
+
+		move(6,1);
+		addstr("Generally you need a lot of the Computer skill and Intelligence before"); 
+		move(7,1);
+		addstr("you can make a successful Hacking attempt. If you are successful there");
+		move(8,1);
+		addstr("is a chance depending on the Computer skill and Intelligence of evading");
+		move(9,1);
+		addstr("being criminalized. If you are criminalised heat on the activist");
+		move(10,1);
+		addstr("increases. Additionally if successful it will increase juice.");
+
+		move(12,1);
+		addstr("Occasionally instead of effecting public opinion your hacker");
+		move(13,1);
+		addstr("may find files that can be used with the Liberal Guardian.");
+	}
+
+	set_color(COLOR_WHITE,COLOR_BLACK,0);
+	move(20,5);
+	addstr("Press any key - Return to previous screen");
+	getch();
+}
