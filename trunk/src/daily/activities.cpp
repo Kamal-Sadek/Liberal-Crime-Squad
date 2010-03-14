c6c367900044bebe387b77ca921d8cc80bdfd1b9
Kurper <Kurper@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2010-03-14 19:07:48 +0000

    okay that actually didn't fix it because I don't understand code, reverting for now but will look at more later

--- trunk/src/daily/activities.cpp
+++ trunk/src/daily/activities.cpp
@@ -2179,11 +2179,7 @@ void funds_and_trouble(char &clearformess)
          delete pool[p];
          pool.erase(pool.begin() + p);
 
-         int burials = bury.size(); // in case someone's killed while doing it
-         // if that happens, just assume they got killed at the burial site, so 
-         // their body doesn't need to be buried later
-
-         for(int b=0;b<burials;b++)
+         for(int b=0;b<bury.size();b++)
          {
             if(!(bury[b]->skill_check(SKILL_STREETSENSE,DIFFICULTY_AVERAGE)))
             {
