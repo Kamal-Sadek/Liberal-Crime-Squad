5a22fd3f0de0ee83062c1cf47393b41ab8bcdedb
blomkvist <blomkvist@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2010-06-12 13:27:45 +0000

    A few changes to the sorting.

--- trunk/src/common/commonactions.cpp
+++ trunk/src/common/commonactions.cpp
@@ -886,9 +886,9 @@ void sleeperize_prompt(Creature &converted, Creature &recruiter, int y)
 }
 
 /* common - Sort a list of creatures.*/
-void sortliberals(std::vector<Creature *>& liberals, short sortingchoice, bool sortdefault)
+void sortliberals(std::vector<Creature *>& liberals, short sortingchoice, bool dosortnone)
 {
-   if(!sortdefault && sortingchoice==SORTING_DEFAULT)return;
+   if(!dosortnone && sortingchoice==SORTING_NONE)return;
    bool nochange = false;
    bool swap = false;
    unsigned loopnr = 0;
@@ -901,8 +901,21 @@ void sortliberals(std::vector<Creature *>& liberals, short sortingchoice, bool s
          swap = false;
          switch (sortingchoice)
          {
-            case SORTING_DEFAULT:
-               swap = liberals[i]->id > liberals[i+1]->id; //I guess this is equivalent to the default. -blomkvist
+            case SORTING_NONE: //This will sort sorted back to unsorted.
+               swap = getpoolcreature(liberals[i]->id) > getpoolcreature(liberals[i+1]->id);
+               /*for (unsigned j=0; j<pool.size(); ++j)
+               {
+                  if (pool[j] == liberals[i])
+                  {
+                     swap = false;
+                     break;
+                  }
+                  else if (pool[j] == liberals[i+1])
+                  {
+                     swap = true;
+                     break;
+                  }
+               }*/
                break;
             case SORTING_NAME:
                swap = strcmp(liberals[i]->name,liberals[i+1]->name)>0;
@@ -913,14 +926,28 @@ void sortliberals(std::vector<Creature *>& liberals, short sortingchoice, bool s
                            && strcmp(liberals[i]->name,liberals[i+1]->name)>0));
                break;
             case SORTING_SQUAD_OR_NAME:
-               swap = ((liberals[i]->squadid == -1 && liberals[i+1]->squadid != -1)
+               swap = ((liberals[i]->squadid == -1 && liberals[i+1]->squadid != -1) //Put squad member above squadless.
                         || (liberals[i+1]->squadid != -1
-                            && liberals[i]->squadid > liberals[i+1]->squadid)
+                            && liberals[i]->squadid > liberals[i+1]->squadid) //Put older squads above newer.
                         || (liberals[i]->squadid == -1
                             && liberals[i+1]->squadid == -1
-                            && strcmp(liberals[i]->name,liberals[i+1]->name)>0)
-                        || (liberals[i]->squadid == liberals[i+1]->squadid 
-                            && strcmp(liberals[i]->name,liberals[i+1]->name)>0)); //This last one should rather compare their order in the squad. -blomkvist
+                            && strcmp(liberals[i]->name,liberals[i+1]->name)>0)); //Sort squadless by name.
+               if (liberals[i]->squadid!=-1 && liberals[i]->squadid == liberals[i+1]->squadid) //Sort members of same squad in the order they are in the squad.
+               {
+                  for (unsigned j=0; j<6; ++j)
+                  {
+                     if (squad[getsquad(liberals[i]->squadid)]->squad[j]->id == liberals[i]->id)
+                     {
+                        swap = false;
+                        break;
+                     }
+                     else if (squad[getsquad(liberals[i]->squadid)]->squad[j]->id == liberals[i+1]->id)
+                     {
+                        swap = true;
+                        break;
+                     }
+                  }
+               }
                break;
             default: break;
          }
@@ -974,7 +1001,7 @@ void sorting_prompt(short listforsorting)
       
       if(c=='a')
       {
-         activesortingchoice[listforsorting]=SORTING_DEFAULT;
+         activesortingchoice[listforsorting]=SORTING_NONE;
          break;
       }
       else if(c=='b')
@@ -992,6 +1019,8 @@ void sorting_prompt(short listforsorting)
          activesortingchoice[listforsorting]=SORTING_SQUAD_OR_NAME;
          break;
       }
+      else if(c==10)
+         break;
    }
 }
 
