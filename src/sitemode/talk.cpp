2952ca6efbc1fc572bf09d5546c22859a5b54834
jonathansfox <jonathansfox@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2013-04-15 21:21:01 +0000

    - Named people you ask out before your first date. Because it's nice to know the name of the person you're going out with. - Removed requirement to wait multiple days to know the abilities of new recruits.
    Patch from seth--:
    - Added "sort by type" option to the Review and Move Equipment screen.
    - Added location to recruitment and dating screens.
    
    Patch from LethalFeline:
    - Added shortcut to move all equipment at once to the Review and Move Equipment screen.

--- src/sitemode/talk.cpp
+++ src/sitemode/talk.cpp
@@ -1129,6 +1129,7 @@ char doYouComeHereOften(Creature &a, Creature &tk)
 
       Creature *newcr=new Creature;
       *newcr=tk;
+      newcr->namecreature();
 
       newcr->location=a.location;
       newcr->base=a.base;
