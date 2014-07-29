afe3b9ab19cb37734070657f1fa67570edafd91d
blomkvist <blomkvist@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2014-07-29 22:11:26 +0000

    Movies and newspaper graphics should look more like they're supposed to on Linux.

--- trunk/src/news/layout.cpp
+++ trunk/src/news/layout.cpp
@@ -21,15 +21,15 @@ void preparepage(newsstoryst& ns, bool liberalguardian)
             move(y,x);
             if(liberalguardian)
             {
-               set_color(newstops[5][x][y][1],
-                         newstops[5][x][y][2],
+               set_color(translateGraphicsColor(newstops[5][x][y][1]),
+                         translateGraphicsColor(newstops[5][x][y][2]),
                          newstops[5][x][y][3]);
                addch(translateGraphicsChar(newstops[5][x][y][0]));
             }
             else
             {
-               set_color(newstops[pap][x][y][1],
-                         newstops[pap][x][y][2],
+               set_color(translateGraphicsColor(newstops[pap][x][y][1]),
+                         translateGraphicsColor(newstops[pap][x][y][2]),
                          newstops[pap][x][y][3]);
 
                addch(translateGraphicsChar(newstops[pap][x][y][0]));
